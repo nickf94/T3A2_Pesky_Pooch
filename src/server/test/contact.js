2060a7d291fb3f0dcf5f2c9a6e61fa84d8529bfb
Coencidental <cdrexler1999@gmail.com>
2020-01-20 20:24:59 +1000

    Updated/emptied test file

--- src/server/test/contact.js
+++ src/server/test/contact.js
@@ -2,3 +2,4 @@ let mongoose = require("mongoose");
 let Contact = require('../database/models/contact_model');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
+let assert = chai.assert
