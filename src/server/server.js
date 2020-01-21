f7b8192db1ae3716aea835698003431405876bed
Dalep <dalepaulsen.code.social@gmail.com>
2020-01-21 11:28:25 +1000

    chai test for user and installed JOI

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,10 @@ const port = process.env.PORT || 7001;
 connectDB();
 const contactRoutes = require('./routes/api/contacts')
 const userRoutes = require('./routes/api/users')
+const server = ('/server/server.js')
 
 app.use('/users', userRoutes)
 app.use('/contacts', contactRoutes)
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = server; // Need this for tests to connect to server
