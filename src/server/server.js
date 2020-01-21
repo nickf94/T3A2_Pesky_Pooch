ef76320bf1d873f2b67a0b2cea2037a7259ee0cf
Coencidental <cdrexler1999@gmail.com>
2020-01-21 10:46:00 +1000

    Configured morgan

--- src/server/server.js
+++ src/server/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express()
 const port = process.env.PORT || 7001;
+const morgan = require('morgan')
+
 connectDB();
 const contactRoutes = require('./routes/api/contacts')
 const userRoutes = require('./routes/api/users')
-
+app.use(morgan('dev'))
 app.use('/api/users', userRoutes)
 app.use('/api/contacts', contactRoutes)
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
