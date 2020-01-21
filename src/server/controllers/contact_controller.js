055d9e22a665a9532b0c42dae57ea1c4d3068803
Coencidental <cdrexler1999@gmail.com>
2020-01-21 11:08:10 +1000

    Contact create functional

--- src/server/controllers/contact_controller.js
+++ src/server/controllers/contact_controller.js
@@ -3,13 +3,15 @@ const Contact = require("./../database/models/contact_model");
 deleteContact = (req, res) => {
   Contact.findByIdAndRemove(req.params.id, req.body)
     .then(contact => res.json({ msg: 'Contact entry deleted successfuly' }))
-    .catch(err => res.status(404).jsob({ error: 'No such contact.'}))
+    .catch(err => res.status(404).json({ error: 'No such contact.'}))
 }
 
-createContact = (req, res) => {
+createContact = async (req, res) => {
+  console.log('test')
+  console.log(req.body)
   Contact.create(req.body)
-  .then(contact => res.json({ contact }))
-  .catch(err => res.status(400).json({ error: 'Unable to send this contact message!'}))
+  .then(contact => res.send(200)) //res.json({ contact }))
+  .catch(err => res.send(400)) //.json({ error: 'Unable to send this contact message!'}))
 }
 
 getContacts = (req, res) => {
--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,14 @@ const app = express()
 const port = process.env.PORT || 7001;
 const morgan = require('morgan')
 
+const bodyParser = require('body-parser')
+
+const bp = 
+
 connectDB();
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 const contactRoutes = require('./routes/api/contacts')
 const userRoutes = require('./routes/api/users')
 app.use(morgan('dev'))
