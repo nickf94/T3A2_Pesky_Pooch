62506ad717bfdaa892ece9511dad08feafc4fb23
nick fletcher <nickfletcher720@gmail.com>
2020-01-22 13:18:45 +1000

    navbar

---
+++ src/client/src/forms/TestimonialForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class loginForm extends Component {
+render() {
+  return(
+    <React.Fragment>
+      <form>
+        <textarea rows="3">
+        </textarea>
+      </form>
+    </React.Fragment>
+  )
+}
+}
