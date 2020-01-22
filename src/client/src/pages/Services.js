b84c0bdfec5f070049060d99101a609fb89c70bb
nick fletcher <nickfletcher720@gmail.com>
2020-01-22 13:03:14 +1000

    review panel added

--- src/client/src/pages/Services.js
+++ src/client/src/pages/Services.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import EventForm from '../forms/EventForm'
-
+import TestimonialForm from '../forms/TestimonialForm'
 
 export const Services  = () => (
   <React.Fragment>
     <div>
       <h1>Services page</h1>
       <EventForm />
+    </div>
+    <div>
+      <TestimonialForm />
     </div>
   </React.Fragment>
 )
