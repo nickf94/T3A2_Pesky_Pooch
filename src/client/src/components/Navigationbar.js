84c8bbf753745debb1a4ae30f4c13e71839403e4
nick fletcher <nickfletcher720@gmail.com>
2020-01-23 10:38:46 +1000

    moves

---
+++ src/client/src/components/Navigationbar.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar } from 'react-bulma-components';
+import logo from '../assets/logo.jpg'
+import '../styles/styles.scss'
+
+export const Navigationbar = () => (
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-icon" href="https://bulma.io">
+        <img src={logo} width="40" height="40" />
+      </a>
+
+      <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+
+      <div class="navbar-start">
+        <p class="navbar-item">
+          <Link to="/">Home</Link>
+        </p>
+
+        <p class="navbar-item">
+          <Link to="/about">About</Link>
+        </p>
+
+        <a class="navbar-item">
+          <Link to="/services">Services</Link>
+        </a>
+        <a class="navbar-item">
+          <Link to="/contact">Contact</Link>
+        </a>
+      </div>
+    </div>
+  </nav>
+)
