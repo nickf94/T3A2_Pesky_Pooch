ba4871edfc86089b15cbe1657fb5be8011314177
Coencidental <cdrexler1999@gmail.com>
2020-01-31 09:59:32 +1000

    Some styling, reorganisation of credentials, beginning to refactor towards only using env variables

--- src/client/src/components/Footer.js
+++ src/client/src/components/Footer.js
@@ -4,11 +4,9 @@ import '../styles/styles.scss'
 export default class Footer extends Component {
   render() {
     return(
-      <footer>
-        <div  className="footer">
+      <footer className="footer">
           <p>Copyright Pesky Pooch ©2020</p>
           <i className="fab fa-facebook" />
-        </div>
       </footer>
     )
   }
