edf2b2768f96601eb962ca038abe8462ea4dd0c0
Dalep <dalepaulsen.code.social@gmail.com>
2020-01-31 14:11:00 +1000

    created event testing branch and skeloton file with imports

--- src/server/config/testConfig.js
+++ src/server/config/testConfig.js
@@ -5,6 +5,7 @@ const app = require("../server");
 const User = require("../database/models/user_model");
 const Testimonial = require ('../database/models/testimonial_model')
 const Contact = require('../database/models/contact_model')
+const Event = require('../database/models/event_model')
 // chai imports
 const chai = require("chai");
 const assert = require("chai").assert;
@@ -12,4 +13,4 @@ const chaiHttp = require("chai-http");
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-module.exports = { app, User, chai, assert, chaiHttp, expect, Testimonial, Contact };
+module.exports = { app, User, chai, assert, chaiHttp, expect, Testimonial, Contact, Event };
