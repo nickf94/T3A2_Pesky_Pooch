0d9a59b3d9a0f3af5d68a40ba117dc0a089c4807
Dalep <dalepaulsen.code.social@gmail.com>
2020-01-31 13:46:05 +1000

    All contact files are tested successfully

--- src/server/config/testConfig.js
+++ src/server/config/testConfig.js
@@ -1,10 +1,15 @@
-// This file holds imports needed to keep test files DRY.
+// Imports needed for tests... to keep code DRY.
+
+// file imports
 const app = require("../server");
 const User = require("../database/models/user_model");
+const Testimonial = require ('../database/models/testimonial_model')
+const Contact = require('../database/models/contact_model')
+// chai imports
 const chai = require("chai");
 const assert = require("chai").assert;
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
 chai.use(chaiHttp);
 
-module.exports = { app, User, chai, assert, chaiHttp, expect };
+module.exports = { app, User, chai, assert, chaiHttp, expect, Testimonial, Contact };
