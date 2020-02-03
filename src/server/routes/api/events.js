f75d06e20d42a3456de368b7a137cb4d398f1da7
Coencidental <cdrexler1999@gmail.com>
2020-02-04 09:11:46 +1000

    Further added requirements for multer so routes can process images

--- src/server/routes/api/events.js
+++ src/server/routes/api/events.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const eventController = require('../../controllers/event_controller.js')
+const multer = require('multer')
+const storage = multer.memoryStorage();
+const upload = multer({ storage })
+
 
 router.get('/test', (req, res) => res.send('Test Event API route'))
 router.get('/', eventController.getEvents)
-router.post('/new', eventController.newEvent)
 router.delete('/delete', eventController.deleteEvent)
 router.put('/update', eventController.updateEvent)
+
+router.post('/new', upload.single('image'), eventController.newEvent)
 
 module.exports = router
--- src/server/routes/api/services.js
+++ src/server/routes/api/services.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const servicesController = require('../../controllers/services_controller.js')
-
+const multer = require('multer')
+const storage = multer.memoryStorage();
+const upload = multer({ storage })
 
 router.get('/test', (req, res) => res.send('Test Services API route'))
 router.get('/', servicesController.getServices)
-router.post('/new', servicesController.newService)
+router.post('/new', upload.single('image'), servicesController.newService)
 router.put('/edit', servicesController.editService)
 router.delete('/delete', servicesController.deleteService)
 module.exports = router
