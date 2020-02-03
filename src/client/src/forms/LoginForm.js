c0c7f2c3482650b1e11f5d33ac27fa716921a226
nick fletcher <nickfletcher720@gmail.com>
2020-02-03 11:59:09 +1000

    final fixes

--- src/client/src/forms/LoginForm.js
+++ src/client/src/forms/LoginForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
-import '../styles/styles.scss'
 
 
 export default class loginForm extends Component {
