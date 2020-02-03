97fe60e14ad9016c9a67107ac41cd35a1fb820b6
nick fletcher <nickfletcher720@gmail.com>
2020-02-03 11:49:22 +1000

    testing contactform

--- src/client/cypress/integration/examples/login.spec.js
+++ src/client/cypress/integration/examples/login.spec.js
@@ -1,14 +1,15 @@
 describe("Test login", () => {
   it("Should go to the login page", () => {
     cy.viewport(1024, 768);
-    cy.visit(process.env.BASE_URL || "http://localhost:3000/");
-    cy.contains("/Home").click();
+    cy.visit(process.env.BASE_URL || "localhost:3000");
+    cy.contains("Login").click();
+    cy.url().should("include", "/login")
   });
 
   it("Should render SignIn component", () => {
-    cy.contains("/login").click();
+    cy.contains("Login").click();
     cy.root()
       .should("contain", "Email")
       .should("contain", "Password");
-  })
-})
+  });
+});
